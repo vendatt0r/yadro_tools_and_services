5f60491b25fcd4e577a31c61ed209cbd38bec6f6
vendatt0r <vasyok03020454@gmail.com>
2025-05-24 20:14:13 +0300

    Обновление логики

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine
 import os
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:123@localhost:5432/yadro_tools")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./yadro_tools.db")
 engine = create_engine(DATABASE_URL, echo=False)
 
 def create_db():
